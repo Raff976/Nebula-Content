41c3fcd3d630a92296b0bc278a3125158bef7be3
raffaello sos <Raff976>
2024-03-20 09:39:39 -0400

    Homework

---
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/assignments/Variable-Homework.js
@@ -0,0 +1,36 @@
+//SUBMIT A LINK TO THE GITHUB REPOSITORY CONTAINING THIS ASSIGNMENT
+// Create a .js file that satisfies assignment below
+// Copy the final assignment text into this assignment to submit
+// For each exercise you'll want to log the result to the console to check your work
+
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+const animalsName = "Zebra"
+console.log(animalname)
+const variablesDefine = "blackandwhitestripes"
+console.log(variablesDefine)
+const AnimalsName = "Girafe"
+console.log(AnimalsName)
+const variablessDefine = "Long neck"
+console.log(variablesDefine)
+const VariablesDefine = "Yellowcolor"
+console.log(variablesDefine)
+const variablesdefine= "longlegs"
+console.log(VariablesDefine)
+
+
+
+
+
+//2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+const carsName = "Honda"
+console.log(carsName)
+const HondaType = "Honda Civic"
+console.log(HondaType)
+const carName = "Fordexplorer"
+console.log(carsName)
+const FordType = "FordF250superduty"
+console.log(FordType)
+const FordEgineF250superduty= "V8Egine"
+console.log(FordEgineF250superduty)
+const FordF250superduty = "dublewheeltiredoublesupportedrearaxle"
+console.log(FordF250superduty)

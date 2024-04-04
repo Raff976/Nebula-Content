b47dbbcd0c7b6f1c85e96e91a9d09cc768c3d3e9
raffaello sos <Raff976>
2024-04-03 22:01:25 -0400

    Homework

--- Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/another-test.js
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/another-test.js
@@ -1,13 +1,36 @@
 console.log ("myfirstlog")
 
-for (let i = 0; i <= 100; i++) {
-    if (i === 10) {
-        break;
+let moose = {
+    name: "Mike",
+    age: 15,
+    weight: 500, // in kilograms
+    eat: function(food) {
+        if (food === "grass") {
+            this.weight += 5; // Assume 5 kg gained by eating grass
+            return `${this.name} ate some grass and gained weight. He now weighs ${this.weight} kg.`;
+        } else {
+            return `${this.name} doesn't eat ${food}.`;
+        }
     }
-    console.log(i);
+};
+
+console.log(moose.eat("grass")); 
+console.log(moose.eat("leaves")); 
+
+
+function addPropertyToObject(obj, propName, propValue) {
+    obj[propName] = propValue;
 }
 
+let Moose = {
+    name: "Mike",
+    age: 15,
+    weight: 500 // in kilograms
+};
 
+addPropertyToObject(moose, "color", "brown");
+
+console.log(moose);
 
 
 
